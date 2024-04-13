43e621d5e76a5ebdb8ea87f9ef5bdbc011817112
Manik <adamgrotkovsky@gmail.com>
2024-04-13 10:36:47 +0200

    Fix reponsive issues, modal issues

--- public/script.js
+++ public/script.js
@@ -1,7 +1,14 @@
 function handleSubmit(e) {
 	e.preventDefault();
 
+	var imageContainer = document.querySelector('.imagearea-1');
 	const ves = this.querySelector("textarea").value;
+
+	if (!ves) {
+		return;
+	}
+
+	imageContainer.appendChild(imageToRemove);
 	const width = document.querySelector("section:nth-child(2)").clientWidth;
 
 	const formular = new URLSearchParams();
@@ -16,6 +23,22 @@ function handleSubmit(e) {
 			document.querySelector("#output").src = URL.createObjectURL(image);
 		})
 		
+	var modal = document.getElementById("myModal");
+	var img = document.getElementById("output");
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	img.style.cursor = "pointer";
+	img.onclick = function(){
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		captionText.innerHTML = this.alt;
+	}
+	
+	var span = document.getElementsByClassName("close")[0];
+	span.onclick = function() { 
+		modal.style.display = "none";
+	}
+
 	document.getElementById("errorMessage").style.display="none";
 }
 
@@ -23,6 +46,10 @@ function clearInput() {
     document.getElementById("ves_text").value = "";
 	document.getElementById("output").removeAttribute("src");
 	document.getElementById("errorMessage").style.display="none";
+
+	var imageToRemove_new = document.getElementById('output');
+    var imageContainer = document.querySelector('.imagearea-1');
+	imageContainer.removeChild(imageToRemove_new);
 }
 
 function downloadImage() {
@@ -59,6 +86,7 @@ function shareImage() {
 	}
 }
 
+const imageToRemove = document.getElementById('output');
 document.querySelector("form").addEventListener("submit", handleSubmit);
 document.getElementById('clear').addEventListener('click', clearInput);
 document.getElementById('download').addEventListener('click', downloadImage);
